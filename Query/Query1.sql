e8e8ffe70b56556604cf15722880ea0cedac8879
ncbidari <nc.bidari@gmail.com>
2023-09-10 01:19:03 -0400

    Query1

---
+++ Query/Query1.sql
@@ -0,0 +1,6 @@
+SELECT film.title
+FROM film
+JOIN inventory ON film.film_id = inventory.film_id
+JOIN rental ON inventory.inventory_id = rental.inventory_id
+JOIN customer ON rental.customer_id = customer.customer_id
+WHERE customer.customer_id = 1;
